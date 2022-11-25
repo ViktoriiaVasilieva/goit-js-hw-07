1ec2c650e60e92b9ed90f2d491fd34e2961432bc
vesnika <vasiliyeva93@gmail.com>
2022-11-26 01:28:32 +0200

    first task

---
+++ js/01-gallery.js
@@ -0,0 +1,39 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallaryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGallaryItemMarkup(galleryItems);
+
+gallaryContainer.addEventListener("click", event => {
+  event.preventDefault();
+  const openModal = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" ">
+`);
+
+  openModal.show();
+});
+
+function onEscKeyPress(evt) {
+  console.log("evt", evt);
+}
+
+gallaryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGallaryItemMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+console.log(galleryItems);
